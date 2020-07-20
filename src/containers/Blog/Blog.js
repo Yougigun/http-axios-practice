1b4bd9c6b68076e627ecd63d9112ed9077e95745
Yougigun <s14031403@gmail.com>
2020-07-21 07:25:39 +0800

    162. Creating a Http Request to GET Data

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
-
+import axios from 'axios'
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
 import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
+    componentDidMount(){
+        axios.get("https://jsonplaceholder.typicode.com/posts")
+            .then(response=>{
+                console.log(response)
+            })
+        
+    }
     render () {
         return (
             <div>
