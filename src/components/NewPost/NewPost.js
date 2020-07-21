16a5348249c72332b601f70ac63ce5c5c06de131
Yougigun <s14031403@gmail.com>
2020-07-21 20:25:29 +0800

    173. Setting a Default Global Configuration for Axios

--- src/components/NewPost/NewPost.js
+++ src/components/NewPost/NewPost.js
@@ -14,7 +14,7 @@ class NewPost extends Component {
             body: this.state.content,
             author:this.state.author
         }
-        axios.post("https://jsonplaceholder.typicode.com/posts/",data)
+        axios.post("/posts/",data)
         .then(res => {
             console.log(res)
         })
