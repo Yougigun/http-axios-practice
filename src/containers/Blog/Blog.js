7f3912204365aa1dc91bf61358a2fef0b815e31c
Yougigun <s14031403@gmail.com>
2020-07-23 07:14:09 +0800

    190. Setting Up the Router Package

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,9 +1,6 @@
 import React, { Component } from 'react';
 // import axios from 'axios'
 import axios from '../../axios'
-import Post from '../../components/Post/Post';
-import FullPost from '../../components/FullPost/FullPost';
-import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
@@ -61,12 +58,6 @@ class Blog extends Component {
                 </header>
                 <section className="Posts">
                     {posts}
-                </section>
-                <section>
-                    <FullPost id={this.state.selectedPostId} />
-                </section>
-                <section>
-                    <NewPost />
                 </section>
             </div>
         );
