d2e164743b85189749849f83a0871826c25bf62f
Yougigun <s14031403@gmail.com>
2020-07-23 20:17:00 +0800

    192. Preparing the Project For Routing

---
+++ src/containers/Blog/Posts/Posts.js
@@ -0,0 +1,61 @@
+import React, { useState, useEffect } from 'react';
+import axios from "../../../axios"
+import Post from '../../../components/Post/Post'
+import './Posts.css'
+
+function Posts(props) {
+
+    const [state, setstate] = useState({
+        posts:[],
+        selectedPostId:null,
+        error:false
+    })
+    useEffect(() => {
+        axios.get("/posts")
+        .then(response=>{
+            const posts = response.data.slice(0,4)
+            const updatedPosts = posts.map(post=>{
+                return{
+                    ...post,
+                    author:"Gary"
+                }
+            })
+            setstate({...state,posts:updatedPosts})
+        })
+        .catch(error => {
+            console.log(error)
+            setstate({error:true})
+        })
+        return () => {
+            "cleanup"
+        }
+    }, [])
+
+    const postSelectedHandler= (id)=>{
+        setstate({...state,selectedPostId:id})
+    }
+
+    let posts = <p style={{textAlign:"center"}}>Something went wrong!</p>
+    console.log(state)
+    if (!state.error){
+        posts = state.posts.map(
+            post =>{
+                return <Post 
+                    key={post.id} 
+                    title={post.title}
+                    author={post.author}
+                    clicked={()=>postSelectedHandler(post.id)}
+                    />
+            }
+        )
+    }
+    return (
+        <section className="Posts">
+            {posts}
+        </section>
+    )
+}
+
+export default Posts
+
+
